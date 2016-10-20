d584f5ee523c4ae6a6855fef0296b21ab53f0d8f
karthikeshwar <karthikeshwar27@gmail.com>
2016-10-20 12:05:54 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main();
+{
+int n;
+printf("enter the number :");
+scanf("%d",&n);
+if(n>0)
+{
+printf(" the given number is positive");
+}else 
+if(n<0)
+{
+printf(" the given number is negative"); 
+}else
+{
+printf(" the given number is zero");
+}
+return 0;
+}
