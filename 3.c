1b78e2cbf0f1e564d43685bee81d9d437cc1db5c
karthikeshwar <karthikeshwar27@gmail.com>
2016-10-24 15:01:49 +0530

    Add files via upload

---
+++ 3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>;
+
+int main()
+
+{
+
+char c;
+
+printf("\n enter the character");
+
+scanf("%c",&c);
+if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+
+{
+
+printf("\n the given character is a vowel");
+
+}
+
+else
+
+{
+
+printf("\n the given character is not a vowel");
+
+}
+
+return 0;
+
+}
